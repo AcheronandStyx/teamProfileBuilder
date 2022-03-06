2ef916a8b3642895eaa56e560ff316c51ec50194
AcheronandStyx <matthew.danielson@trojans.dsu.edu>
2022-03-06 01:37:47 -0600

    restructuring Classes

--- utils/prompts.js
+++ utils/prompts.js
@@ -2,11 +2,69 @@
 // inquirer prompts to start the process and create a manager for the team
 
 // How to use string literal in an inquirer prompt, like use the employees name in follow up questions?
-const createEmployee = [
+const createManager = [
     {
         type: 'input',
         name: 'employeeName',
         message: 'What is their name?',
+        validate: nameInpout => {
+            if (nameInpout) {
+                return true;
+            } else {
+                console.log('Please enter their name!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is their Employee ID?',
+        validate: idInput => {
+            if (idInput) {
+                return true;
+            } else {
+                console.log('Please enter their employee ID!');
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is their email address?',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('Please enter their email address!');
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'What is the managers office number?', // This only gets asked once when a manager is created
+        validate: officeNumberInput => {
+            if (officeNumberInput) {
+                return true;
+            } else {
+                console.log('Please enter their office number');
+            }
+        }
+    }
+];
+
+const createEmployee = [
+    {
+        type: 'list',
+        name: 'confirmTeam',
+        message: 'Would you like to add a team member or finalize the team?',
+        choices: ['Add an engineer', 'Add an intern', 'Finalize Team']
+    },
+    {
+        type: 'input',
+        name: 'employeeName',
+        message: 'What is the employees name?',
         validate: nameInpout => {
             if (nameInpout) {
                 return true;
@@ -48,44 +106,7 @@ const createEmployee = [
             return  "Please provide a valid email address!"; 
             }
         }*/
-    }
-];
-
-const managerQuery = [
-    {
-        type: 'input',
-        name: 'officeNumber',
-        message: 'What is the managers office number?', // This only gets asked once when a manager is created
-        validate: officeNumberInput => {
-            if (officeNumberInput) {
-                return true;
-            } else {
-                console.log('Please enter their office number'); // Make a seperate function to validate an email address was entered??? Regex  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-            }
-        }
-    }
-]
-
-const confirmAddMember = [
-    {
-        type: 'list',
-        name: 'confirmTeam',
-        message: 'Would you like to add a team member or finalize the team?',
-        choices: ['Add an engineer', 'Add an intern', 'Finalize Team']
-    }
-];
-/*
-const confirmEmployeeType = [
-    {
-        type: 'list',
-        name: 'role',
-        message: 'Is the new team member an Enginner or an Intern?',
-        choices: ['Engineer', 'Intern'],
-    }
-];*/
-
-// restrcuture prompts so the confirmTeam questions isthe first question
-const createNonManger = [
+    },
     {
         type: 'input',
         name: 'gitHub',
@@ -119,5 +140,4 @@ const createNonManger = [
 ];
 
 
-
-    module.exports = { managerQuery, createEmployee, confirmAddMember, createNonManger };
+module.exports = { createManager, createEmployee };
